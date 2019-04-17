db25705c9e985bc1bf02986410e9ca518ae25dc9
peteryang1110 <49416678+peteryang1110@users.noreply.github.com>
2019-04-17 14:36:24 -0600

    Update mainClass.cpp
    change the vectors of classes* that are used to store the information of classes to map.

--- mainClass.cpp
+++ mainClass.cpp
@@ -4,15 +4,14 @@
 #include <sstream>
 using namespace std;
 
-Main::Main() {
-}
+Main::Main() {}
 
 Main::Main(string filename) {
   ifstream input;
   input.open(filename);
   string line;
   while (getline(input, line)) {
-    string n, l, t, d;
+    string n, l, t, d;  // name, level, time, day of the week
     stringstream ss;
     ss << line;
     getline(ss, n, ' ');
@@ -31,39 +30,32 @@ Main::Main(string filename) {
     }
     Time* newT = new Time(t, d);
     i = data.find(n);
-    bool flag = false;
-    if (!flag) {
-      vector<Classes*>::iterator c;
-      for (c = i->second[stoi(l) / 1000 - 1]->classes.begin(); c != i->second[stoi(l) / 1000 - 1]->classes.end(); c++) {
-        if ((*c)->level == l) {
-          (*c)->times.push_back(newT);
-          flag = true;
-          break;
-        }
-      }
-    }
-    if (flag == false) {
-      Classes* newClass = new Classes(n, l, newT);
-      i->second[stoi(l) / 1000 - 1]->classes.push_back(newClass);
+    map<string, Classes*>::iterator m = i->second[stoi(l) / 1000 - 1]->classes.find(l);
+    if (m == i->second[stoi(l) / 1000 - 1]->classes.end()) {
+      Classes* newClass = new Classes(n, newT);
+      i->second[stoi(l) / 1000 - 1]->classes.insert(pair<string, Classes*>(l, newClass));
+    } else {
+      m->second->times.push_back(newT);
     }
   }
   input.close();
 }
 
 Main::~Main() {
+  data.clear();
 }
 
 void Main::printAll() {
   map<string, Level**>::iterator i;
-  vector<Classes*>::iterator j;
+  map<string, Classes*>::iterator j;
   for (i = data.begin(); i != data.end(); i++) {
     for (int k = 0; k < 4; k++) {
       if (!i->second[k]->classes.empty()) {
         cout << i->first << " => " << i->second[k]->level << ": " << endl;
         vector<Time*>::iterator t;
         for (j = i->second[k]->classes.begin(); j != i->second[k]->classes.end(); j++) {
-          cout << (*j)->name << (*j)->level;
-          for (t = (*j)->times.begin(); t != (*j)->times.end(); t++) {
+          cout << (*j).second->name << (*j).first;
+          for (t = (*j).second->times.begin(); t != (*j).second->times.end(); t++) {
             cout << " " << (*t)->d << " " << (*t)->t;
           }
           cout << endl;
