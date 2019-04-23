bf361d510c4b40035bac8b58be9223984d5222d5
peteryang1110 <49416678+peteryang1110@users.noreply.github.com>
2019-04-22 19:56:53 -0600

    Update mainClass.cpp
    Add all the functions that are needed for the project

--- mainClass.cpp
+++ mainClass.cpp
@@ -19,7 +19,7 @@ Main::Main(string filename) {
     getline(ss, t, ' ');
     getline(ss, d);
 
-    map<string, Level**>::iterator i = data.find(n);
+    auto i = data.find(n);
     if (i == data.end()) {
       Level** fourLevels = new Level*[4];
       fourLevels[0] = new Level("1000");
@@ -30,7 +30,7 @@ Main::Main(string filename) {
     }
     Time* newT = new Time(t, d);
     i = data.find(n);
-    map<string, Classes*>::iterator m = i->second[stoi(l) / 1000 - 1]->classes.find(l);
+    auto m = i->second[stoi(l) / 1000 - 1]->classes.find(l);
     if (m == i->second[stoi(l) / 1000 - 1]->classes.end()) {
       Classes* newClass = new Classes(n, newT);
       i->second[stoi(l) / 1000 - 1]->classes.insert(pair<string, Classes*>(l, newClass));
@@ -64,4 +64,110 @@ void Main::printAll() {
       }
     }
   }
+}
+
+bool Main::readInput(vector<InputClass> user) {
+  if (user.empty()) {
+    return false;
+  }
+  vector<InputClass>::iterator v;
+  for (v = user.begin(); v != user.end(); v++) {
+    auto m = data.find(v->prefix);
+    if (m == data.end()) {
+      return false;
+    } else {
+      auto c = m->second[stoi(v->number) / 1000 - 1]->classes.find(v->number);
+      if (c == m->second[stoi(v->number) / 1000 - 1]->classes.end()) {
+        return false;
+      } else {
+        Schedule* newS = new Schedule(v->prefix, v->number, c->second->times);
+        // cout << v->prefix << " " << v->number << endl;
+        // for (auto i = c->second->times.begin(); i != c->second->times.end(); i++) {
+        //   cout << (*i)->d << " " << (*i)->t;
+        // }
+        // cout << endl;
+        schedules.push_back(newS);
+      }
+    }
+  }
+  return true;
+}
+
+void Main::inputEdges() {
+  for (auto i = schedules.begin(); i != schedules.end() - 1; i++) {
+    (*i)->next = *(i + 1);
+  }
+  // for (auto i = schedules.begin(); i != schedules.end(); i++) {
+  //   if ((*i)->next != NULL) {
+  //     cout << (*i)->name << " => " << (*i)->next->name << endl;
+  //   } else {
+  //     cout << (*i)->name << " => NULL" << endl;
+  //   }
+  // }
+}
+
+vector<int> startTime(Time* t) {
+  stringstream ss;
+  ss << t->t;
+  string stringStart, stringEnd;
+  getline(ss, stringStart, '-');
+  getline(ss, stringEnd);
+  int intStart = stoi(stringStart) / 100;
+  int intEnd = stoi(stringEnd) / 100;
+  vector<int> timeSlot;
+  for (int i = intStart; i <= intEnd; i++) {
+    timeSlot.push_back(i);
+  }
+  return timeSlot;
+}
+
+void printSchedule(vector<string> option) {
+  for (auto i = option.begin(); i != option.end(); i++) {
+    cout << *i << endl;
+  }
+}
+
+void Main::helper(Schedule* current, vector<string> option, int count, int optionNum) {
+  if (current != NULL) {
+    for (auto i = current->times.begin(); i != current->times.end(); i++) {
+      vector<int> st = startTime(*i);
+      for (int j = 0; j < (*i)->d.length(); j++) {
+        string index = "";
+        if ((*i)->d[j] == 'T' && (*i)->d[j + 1] == 'h') {
+          index = "Th";
+        } else {
+          index += (*i)->d[j];
+        }
+        for (auto k = st.begin(); k != st.end(); k++) {
+          if (slot[index].find(*k) == slot[index].end()) {
+            continue;
+          } else {
+            return;
+          }
+        }
+        for (auto k = st.begin(); k != st.end(); k++) {
+          slot[index].insert(*k);
+        }
+      }
+      string classInfo = current->name + current->level + " " + (*i)->d + " " + (*i)->t;
+      option.push_back(classInfo);
+      helper(current->next, option, count, optionNum);
+      if (option.size() == count) {
+        cout << "Option " << optionNum << ":" << endl;
+        printSchedule(option);
+        cout << endl;
+        optionNum++;
+      }
+    }
+    option.pop_back();
+  }
+}
+
+void Main::computeSchedule(int count) {
+  vector<string> option;
+  int optionNum = 1;
+
+  helper(*schedules.begin(), option, count, optionNum);
+  cout << endl
+       << "(If there is no option printed out, that means it is impossible to have all the classes at once)" << endl;
 }
